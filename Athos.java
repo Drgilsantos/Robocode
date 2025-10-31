c6fc38a743c10f7cd4eab038052143808428cd4d
Gilberto <gilcsster@gmail.com>
2025-10-30 22:31:45 -0300

    Atualizações especiais

---
+++ Athos.java
@@ -0,0 +1,66 @@
+package Athos;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+/**
+ * Athos - a robot by (your name here)
+ */
+public class Athos extends Robot
+{
+	/**
+	 * run: Athos's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(1000);
+			turnRight(180);
+			back(200);
+			turnLeft(270);
+			ahead(1000);
+			
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		double distance = e.getDistance();
+		double firePower = Math.min(400 / distance, 3);
+		fire(firePower); 
+        
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+		turnRight(90); 
+		ahead(1000);
+		turnRight(90);
+		turnGunRight(180);	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		turnRight(90);
+		ahead(1000);
+		turnRight(90);
+	}	
+}
